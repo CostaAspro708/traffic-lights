117b9f4ea45943b19c2c1d157d45ac159bef9a77
Costa A <cgaspro2@gmail.com>
2021-08-28 08:30:04 +1000

    Initial commmit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+//Traffic light component 
+function Traffic_Light() {
+//Variable on used to keep track of the components state.
+//on = 1 means green light, on = 0 means red.
+ const [on, SetOn] = useState(1);
+
+ //When container is clicked change on.
+ if(on == 0){
+   return(
+    <div className="traffic_light_container" onClick={() => SetOn(1)}>
+      <div className="off_light"></div>
+      <div className="green_light"></div>
+    </div>
+    )
+ }else if(on == 1){
+  return(
+    <div className="traffic_light_container" onClick={() => SetOn(0)}>
+      <div className="red_light"></div>
+      <div className="off_light"></div>
+    </div>
+    )
+ }  
+}
 
 function App() {
   return (
     <div className="App">
+       <h1> Simply click the traffic light to change the colour!</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <Traffic_Light/>
+        <Traffic_Light/>
       </header>
     </div>
   );
